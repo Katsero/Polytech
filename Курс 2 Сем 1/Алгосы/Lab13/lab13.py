f4ab9cf837a3a3cf76727d0dd1300e01f79d6c77
Katsero <raleksandrd25@gmail.com>
2025-11-26 22:10:22 +0300

    algosi lab13 node removal

--- Курс 2 Сем 1/Алгосы/Lab13/lab13.py
+++ Курс 2 Сем 1/Алгосы/Lab13/lab13.py
@@ -10,12 +10,16 @@ class Tree():
         for elem in arr:
             self.treeAppend(elem)
 
+    # # ERRORS
+    def enf(self):
+        print('Element not found')
+
     def treeAppend(self, value):
         if not self.root:
             self.root = Node(value)
             return
         current = self.root
-        while current:
+        while True:
             if value < current.value:
                 if not current.left:
                     current.left = Node(value)
@@ -29,33 +33,152 @@ class Tree():
                 return
             # if current.right
             current = current.right
+            continue
 
-    def treeOutput(self, node = None, depth = 0, res=[]):
-        if depth == 0:
-            node = self.root
+# # Far not the best attempt of removal
+    # def treeRemove(self, value):
+    #     # # root check
+    #     if not self.root:
+    #         self.nef()
+    #         return
 
-        if not res[depth]:
-            res[depth] = []
+    #     # # root removal
+    #     if self.root.value == value:
+    #         current = self.root.right
+    #         if current:
+    #             if current.left:
+    #                 while current.left.left:
+    #                     current = current.left
+    #                 if current.left.right:
+    #                     current.left, current.left.left, current.left.right, self.root = current.left.right, self.root.left, self.root.right, current.left
+    #                     return
+    #                 # if not current.left.right
+    #                 current = current.left
+    #                 current.left, current.right, self.root = self.root.left, self.root.right, self.root
+    #                 return
+    #             # if not current.left
+    #             current.left, self.root = self.root.left, current
+    #             return
+    #         # if not self.root.right
+    #         self.root = self.root.left
+    #         return
 
-        if node:
-            res[depth].append(node.value)
-            res = self.treeOutput(node.left,depth,res)
-            res = self.treeOutput(node.right,depth,res)
+    #     # # node removal
+    #     current = self.root
+    #     while current:
+    #         if value < current.value:
+    #             if current.left and current.left.value == value:
+    #                 if current.left.left and current.left.right: # both nodes
+    #                     if current.left.right.left:
+    #                         while current.left.right.left.left:
+    #                             pass
+    #                         return
+    #                     current.left, current.left.right.left = current.left.right, current.left.left
+    #                     return
+    #                 #if nodes lack
+    #                 current.left = (current.left.left if current.left.left else current.left.right) #take existing one
+    #             # if not current.left or not current.left.value == value
+    #             current = current.left
+    #             continue
+    #         # if value > current.value
+    #         if current.right and current.right.value == value:
+    #             # TODO remove
+    #             return
+    #         # if not current.right or not current.right.value == value
+    #         current = current.right
+    #         continue
+
+    def treeRemove(self, value):
+        if not self.root:
+            self.enf()
+            return
         
-        if depth == 0:
-            print(res)
-        else:
-            return(res)
-    # def treeOutput(self, node=None, depth=0):
+        # if root
+        if self.root.value == value:
+            if not self.root.left or not self.root.right: # if 0/1 node
+                self.root = (self.root.left if self.root.left else self.root.right)
+                return
+            # if 2 nodes
+            current = self.root.right
+            if not current.left: # right is min
+                self.root, current.left = current, self.root.left
+                return
+            while current.left.left: #Ищем, current.left - минимальный элемент, не имеет левого потомка (!)
+                current = current.left
+            current.left.left, current.left.right, current.left, self.root = current.left, self.root.left, self.root.right, current.left.right
+            return
+            
+        # if not root
+        current = self.root
+        while current:
+            if value < current.value:
+                if  current.left and current.left.value == value:
+                    if not current.left.left or not current.left.right: # if 0/1 node
+                        current.left = (current.left.left if current.left.left else current.left.right)
+                        return
+                    # if 2 nodes
+                    minimal = current.left.right
+                    if not minimal.left: #right is min
+                        current.left,minimal.left = minimal, current.left.left
+                    while minimal.left.left: # minimal.left будет минимальным
+                        minimal = minimal.left
+                    minimal.left.left, minimal.left.right, minimal.left, current.left = minimal.left.right, minimal.left, current.left.left, current.left.right
+                current = current.left
+            if value > current.value:
+                if  current.right and current.right.value == value:
+                    if not current.right.left or not current.right.right: # if 0/1 node
+                        current.right = (current.right.left if current.right.left else current.right.right)
+                        return
+                    # if 2 nodes
+                    minimal = current.right.right
+                    if not minimal.left: #right is min
+                        current.right,minimal.left = minimal, current.right.left
+                    while minimal.left.left: # minimal.left будет минимальным
+                        minimal = minimal.left
+                    minimal.left.left, minimal.left.right, minimal.left, current.right = minimal.left.right, minimal.left, current.right.left, current.right.right
+                    return
+                current = current.right
+        self.enf()
+        return
+            
+
+        
+# # Far not the best attempt of output
+    # def treeOutput(self, node = None, depth = 0, res=[]):
     #     if depth == 0:
     #         node = self.root
 
+    #     if not res[depth]:
+    #         res[depth] = []
+
     #     if node:
-    #         print("  "*depth, node.value)
-    #         self.treeOutput(node.right, depth+1)
-    #         self.treeOutput(node.left, depth+1)
+    #         res[depth].append(node.value)
+    #         res = self.treeOutput(node.left,depth,res)
+    #         res = self.treeOutput(node.right,depth,res)
+        
+    #     if depth == 0:
+    #         print(res)
+    #     else:
+    #         return(res)
+        
+    def treeOutput(self, node=None, depth=0):
+        
+        if depth == 0:
+            print('tree:')
+            node = self.root
+
+        if node:
+            print("  "*depth, node.value)
+            self.treeOutput(node.right, depth+1)
+            self.treeOutput(node.left, depth+1)
+        
+        if depth == 0:
+            print('-------')
 
 newTree = Tree([23, 87, 5, 64, 91, 12, 76, 34, 45, 89])
 print('created')
-print('tree:')
+newTree.treeOutput()
+newTree.treeRemove(23)
+newTree.treeOutput()
+newTree.treeRemove(87)
 newTree.treeOutput()
