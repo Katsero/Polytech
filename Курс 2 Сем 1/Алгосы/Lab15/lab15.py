9fca61ba006a500eedf117385a77ded1364a26e1
Katsero <raleksandrd25@gmail.com>
2025-12-06 01:06:19 +0300

    15 almost done, timer in process

--- Курс 2 Сем 1/Алгосы/Lab15/lab15.py
+++ Курс 2 Сем 1/Алгосы/Lab15/lab15.py
@@ -1,3 +1,4 @@
+from time import *
 from random import *
 def arrayCreate(length = 11):
     ##РАЗМЕРНОСТЬ МАССИВА
@@ -10,55 +11,61 @@ def arrayCreate(length = 11):
 # Кусок с лабы 8
 
 class Pyramide():
-    def __init__(self, tree = None):
+    def __init__(self, tree = None, maximal = True):
+        self.maximal = maximal
         self.tree = []
         if tree:
             for element in tree:
                 self.pyramideAppend(element)
 
+
     # ERRORS
-    def enf(self,value):
+    def enf(self,value = None):
         print(f"Element {value} not found")
         return
     def tie(self):
         print(f'Tree is empty')
         return
+    def loor(self):
+        print(f'List out of range')
+        return
 
+    # Tree immitation
     def findParentIndex(self, index):
         if index <= 1:
             return None
         return index//2
-    
     def findLeftChildIndex(self, index):
         if (index * 2) > len(self.tree):
             return None
         return index*2
-    
     def findRightChildIndex(self, index):
         if ((index * 2)+1) > len(self.tree):
             return None
         return (index*2) + 1
+    
 
-    def pyramideAppend(self,value):
+    # Main functions
+    def pyramideAppend(self, value):
+        if not self.maximal:
+            value = -value
+            
         self.tree += [value]
         
         newIndex = len(self.tree)
-        while self.findParentIndex(newIndex) != None:
-             if not self.pyramideRearrange(newIndex):
-                 break
-             newIndex = self.findParentIndex(newIndex)
+        while self.findParentIndex(newIndex) is not None:
+            parentIndex = self.findParentIndex(newIndex)
+            if self.tree[newIndex - 1] <= self.tree[parentIndex - 1]:
+                break
+            self.tree[newIndex - 1], self.tree[parentIndex - 1] = self.tree[parentIndex - 1], self.tree[newIndex - 1]
+            newIndex = parentIndex 
 
-    def pyramideRearrange(self, childIndex):
-        parentIndex = self.findParentIndex(childIndex)
-        if self.tree[childIndex-1] > self.tree[parentIndex-1]:
-            self.tree[childIndex-1], self.tree[parentIndex-1] = self.tree[parentIndex-1], self.tree[childIndex-1]
-            return True
-        return False
-    
     def pyramideRemove(self,value):
         if not self.tree:
             self.enf(value)
             return    
+
+        value = value if self.maximal else -value
         
         queue = [1]
         res = None
@@ -73,12 +80,67 @@ class Pyramide():
             if self.findRightChildIndex(current):
                 queue.append(self.findRightChildIndex(current))
 
-        if res:
+        value = value if self.maximal else -value
+        if res != None:
             self.tree[res-1] = self.tree[-1]
-            self.tree = self.tree[:-1]
+            self.tree.pop()
+            self.pyramideRearrange(res)
+        else:
+            self.enf(value)
+    
+    def pyramidePop(self, index = 0):
+        if not self.tree:
+            self.loor()
+            return
 
+        if len(self.tree) <= index:
+            self.loor()
+            return
         
+        if index + 1 == len(self.tree):
+            return self.tree.pop()
 
+        head, self.tree[index] = self.tree[index], self.tree[-1]
+        self.tree.pop()
+        self.pyramideRearrange(index+1)
+        return head
+        
+    def pyramideRearrange(self, parentIndex):
+        leftChildIndex = self.findLeftChildIndex(parentIndex)
+        if leftChildIndex and self.tree[parentIndex-1] < self.tree[leftChildIndex-1]:
+            self.tree[parentIndex-1], self.tree[leftChildIndex-1] = self.tree[leftChildIndex-1], self.tree[parentIndex-1]
+            self.pyramideRearrange(leftChildIndex)
+        rightChildIndex = self.findRightChildIndex(parentIndex)
+        if rightChildIndex and self.tree[parentIndex-1] < self.tree[rightChildIndex-1]:
+            self.tree[parentIndex-1], self.tree[rightChildIndex-1] = self.tree[rightChildIndex-1], self.tree[parentIndex-1]
+            self.pyramideRearrange(rightChildIndex)
+
+    def pyramideSearch(self,value):
+        if not self.tree:
+            self.enf(value)
+            return    
+
+        value = value if self.maximal else -value
+        
+        queue = [1]
+        res = []
+        while queue:
+            current = queue.pop(0)
+            if self.tree[current-1] == value:
+                res.append(current-1)
+
+            if self.findLeftChildIndex(current):
+                queue.append(self.findLeftChildIndex(current))
+            if self.findRightChildIndex(current):
+                queue.append(self.findRightChildIndex(current))
+
+        value = value if self.maximal else -value
+        if res:
+            print(f'Elemement {value} found at {res} positions')
+        else:
+            self.enf(value)
+
+    # Output (obviously)
     def pyramideOutput(self, index = 1, depth = 0):
         
         # Вывод с правого поддрева -> корень -> левое поддрево
@@ -99,62 +161,85 @@ class Pyramide():
 
         if self.findRightChildIndex(index):
             self.pyramideOutput(self.findRightChildIndex(index), depth + 1)
-        print('    '*depth, self.tree[index-1])
+        print('    '*depth, self.tree[index-1] if self.maximal else -self.tree[index-1])
         if self.findLeftChildIndex(index):
             self.pyramideOutput(self.findLeftChildIndex(index), depth + 1)
 
         if depth == 0:
             print('-------')
-
-    def pyramideSearch(self,value):
-        if not self.tree:
-            self.enf(value)
-            return    
         
-        queue = [1]
-        res = []
-        while queue:
-            current = queue.pop(0)
-            if self.tree[current-1] == value:
-                res.append(current-1)
-
-            if self.findLeftChildIndex(current):
-                queue.append(self.findLeftChildIndex(current))
-            if self.findRightChildIndex(current):
-                queue.append(self.findRightChildIndex(current))
-
-        if res:
-            print(f'Elemement {value} found at {res} positions')
-        else:
-            self.enf(value)
-        
+    # Sort
     def pyramideSort(self):
-        # TODO
         res = []
+        while self.tree:
+            
+            head = self.pyramidePop()
+            res.append(head if self.maximal else -head)
         return res
 
 
 # newPyramide = Pyramide([45,2,16,19,81,54,47,15,65])
-newPyramide = Pyramide(arrayCreate())
-newPyramide.pyramideOutput()
-print(newPyramide.tree)
-newPyramide.pyramideSearch(newPyramide.tree[4])
-newPyramide.pyramideSearch('aboba')
-newPyramide.pyramideOutput()
-print(newPyramide.tree)
-newPyramide.pyramideRemove(newPyramide.tree[3])
-newPyramide.pyramideOutput()
-print(newPyramide.tree)
+arr = arrayCreate()
 
+## max
+newPyramide = Pyramide(arr)
+newPyramide.pyramideOutput()
+print('-------')
+newPyramide.pyramideSearch(newPyramide.tree[4])
+newPyramide.pyramideSearch(46064)
+newPyramide.pyramideOutput()
+print('-------')
+newPyramide.pyramideRemove(newPyramide.tree[0])
+newPyramide.pyramideRemove(46064)
+newPyramide.pyramideOutput()
+print('-------')
+
+## min
+newPyramide = Pyramide(arr, False)
+newPyramide.pyramideOutput()
+print('-------')
+newPyramide.pyramideSearch(-newPyramide.tree[4])
+newPyramide.pyramideSearch(46064)
+newPyramide.pyramideOutput()
+print('-------')
+newPyramide.pyramideRemove(-newPyramide.tree[0])
+newPyramide.pyramideRemove(46064)
+newPyramide.pyramideOutput()
+print('-------')
+
+## empty
 newPyramide = Pyramide()
 newPyramide.pyramideOutput()
-newPyramide.pyramideSearch('aboba')
+newPyramide.pyramideRemove(46064)
+newPyramide.pyramideSearch(46064)
+print('-------')
 
+## head only
 newPyramide = Pyramide([5])
 newPyramide.pyramideOutput()
-newPyramide.pyramideSearch('aboba')
+newPyramide.pyramideSearch(46064)
+print('-------')
 
+
+## sort
+newPyramide = Pyramide(arr)
+newPyramide.pyramideOutput()
+sortedArr = newPyramide.pyramideSort()
+print(sortedArr)
+
+## sort min
+newPyramide = Pyramide(arr, False)
+newPyramide.pyramideOutput()
+sortedArr = newPyramide.pyramideSort()
+print(sortedArr)
+
+
+#Function timer
+def Timer(array,func,number):
+    start_time = time()
+    print(func.__name__)
+    func(array[::],number)
+    end_time = time()
+    print(end_time - start_time)
 # TODO:
-# a: мин/макс пирамида - выбор, 
-# b: удаление
-# c: пирамидальная сортировка, время выполнения
+# c: время выполнения
