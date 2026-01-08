fe140b1eef012a8d8b1a04359d326450f6e45956
Katsero <raleksandrd25@gmail.com>
2026-01-08 18:03:32 +0300

    heap 21 ab in process

--- Курс 2 Сем 1/Алгосы/Lab21/lab15.py
+++ Курс 2 Сем 1/Алгосы/Lab21/lab15.py
@@ -1,241 +1,87 @@
-from time import *
-from random import *
-def arrayCreate(length = 11):
-    ##РАЗМЕРНОСТЬ МАССИВА
-    array_range = 100
-    array = []
-    for _ in range(length):
-        array.append(randint(0, array_range))
-        # array.append(randint(-array_range, array_range))
-    return array
-# Кусок с лабы 8
+import time
+import random
 
-class Pyramide():
-    def __init__(self, tree = None, maximal = True):
+class Pyramide:
+    def __init__(self, arr=None, maximal=True):
         self.maximal = maximal
         self.tree = []
-        if tree:
-            for element in tree:
-                self.pyramideAppend(element)
+        if arr:
+            for element in arr:
+                self._append_no_log(element)
 
-
-    # ERRORS
-    def enf(self,value = None):
-        print(f"Element {value} not found")
-        return
-    def tie(self):
-        print(f'Tree is empty')
-        return
-    def loor(self):
-        print(f'List out of range')
-        return
-
-    def findParentIndex(self, index):
-        if index <= 1:
-            return None
-        return index//2
-    def findLeftChildIndex(self, index):
-        if (index * 2) > len(self.tree):
-            return None
-        return index*2
-    def findRightChildIndex(self, index):
-        if ((index * 2)+1) > len(self.tree):
-            return None
-        return (index*2) + 1
-    
-
-    def pyramideAppend(self, value):
-        if not self.maximal:
-            value = -value
-            
-        self.tree += [value]
-        
-        newIndex = len(self.tree)
-        while self.findParentIndex(newIndex) is not None:
-            parentIndex = self.findParentIndex(newIndex)
-            if self.tree[newIndex - 1] <= self.tree[parentIndex - 1]:
+    def _append_no_log(self, value):
+        val = value if self.maximal else -value
+        self.tree.append(val)
+        idx = len(self.tree)
+        while idx > 1:
+            parent = idx // 2
+            if self.tree[idx - 1] <= self.tree[parent - 1]:
                 break
-            self.tree[newIndex - 1], self.tree[parentIndex - 1] = self.tree[parentIndex - 1], self.tree[newIndex - 1]
-            newIndex = parentIndex 
+            self.tree[idx - 1], self.tree[parent - 1] = self.tree[parent - 1], self.tree[idx - 1]
+            idx = parent
 
-    def pyramideRemove(self,value):
-        if not self.tree:
-            self.enf(value)
-            return    
-
-        value = value if self.maximal else -value
-        
-        queue = [1]
-        res = None
-        while queue:
-            current = queue.pop(0)
-            if self.tree[current-1] == value:
-                res = current
-                break
-
-            if self.findLeftChildIndex(current):
-                queue.append(self.findLeftChildIndex(current))
-            if self.findRightChildIndex(current):
-                queue.append(self.findRightChildIndex(current))
-
-        value = value if self.maximal else -value
-        if res != None:
-            self.tree[res-1] = self.tree[-1]
-            self.tree.pop()
-            self.pyramideRearrange(res)
-        else:
-            self.enf(value)
-    
-    def pyramidePop(self, index = 0):
-        if not self.tree:
-            self.loor()
-            return
-
-        if len(self.tree) <= index:
-            self.loor()
-            return
-        
-        if index + 1 == len(self.tree):
-            return self.tree.pop()
-
-        head, self.tree[index] = self.tree[index], self.tree[-1]
-        self.tree.pop()
-        self.pyramideRearrange(index+1)
-        return head
-        
     def pyramideRearrange(self, parentIndex):
         while True:
             largest = parentIndex
-            left = self.findLeftChildIndex(parentIndex)
-            right = self.findRightChildIndex(parentIndex)
+            left = parentIndex * 2
+            right = parentIndex * 2 + 1
 
-            if left is not None and self.tree[left - 1] > self.tree[largest - 1]:
+            if left <= len(self.tree) and self.tree[left - 1] > self.tree[largest - 1]:
                 largest = left
-            if right is not None and self.tree[right - 1] > self.tree[largest - 1]:
+            if right <= len(self.tree) and self.tree[right - 1] > self.tree[largest - 1]:
                 largest = right
 
             if largest == parentIndex:
                 break
 
-            self.tree[parentIndex - 1], self.tree[largest - 1] = \
-                self.tree[largest - 1], self.tree[parentIndex - 1]
-
+            self.tree[parentIndex - 1], self.tree[largest - 1] = self.tree[largest - 1], self.tree[parentIndex - 1]
             parentIndex = largest
 
-    def pyramideSearch(self,value):
-        if not self.tree:
-            self.enf(value)
-            return    
-
-        value = value if self.maximal else -value
-        
-        queue = [1]
-        res = []
-        while queue:
-            current = queue.pop(0)
-            if self.tree[current-1] == value:
-                res.append(current-1)
-
-            if self.findLeftChildIndex(current):
-                queue.append(self.findLeftChildIndex(current))
-            if self.findRightChildIndex(current):
-                queue.append(self.findRightChildIndex(current))
-
-        value = value if self.maximal else -value
-        if res:
-            print(f'Elemement {value} found at {res} positions')
-        else:
-            self.enf(value)
-
-    # Output (obviously)
-    def pyramideOutput(self, index = 1, depth = 0):
-        
-        # Вывод с правого поддрева -> корень -> левое поддрево
-        #         10
-        #     20
-        #         15
-        # 45
-        #         10
-        #     40
-        #         35
-        
-        if not self.tree:
-            self.tie()
-            return
-
-        if depth == 0:
-            print('tree:')
-
-        if self.findRightChildIndex(index):
-            self.pyramideOutput(self.findRightChildIndex(index), depth + 1)
-        print('    '*depth, self.tree[index-1] if self.maximal else -self.tree[index-1])
-        if self.findLeftChildIndex(index):
-            self.pyramideOutput(self.findLeftChildIndex(index), depth + 1)
-
-        if depth == 0:
-            print('-------')
-        
-    # Sort
     def pyramideSort(self):
         res = []
         while self.tree:
-            
-            head = self.pyramidePop()
+            head = self.tree[0]
             res.append(head if self.maximal else -head)
+            last = self.tree.pop()
+            if self.tree:
+                self.tree[0] = last
+                self.pyramideRearrange(1)
         return res
 
 
-# # newPyramide = Pyramide([45,2,16,19,81,54,47,15,65])
-arr = arrayCreate()
+def benchmark_heap():
+    print("=== Пирамидальная сортировка (только pyramideSort) ===")
+    n = int(input("Число элементов (N): "))
+    value_range = int(input("Диапазон значений (0 до RANGE): "))
+    iterations = int(input("Число итераций: "))
 
-## max
-newPyramide = Pyramide(arr)
-newPyramide.pyramideOutput()
-# print('-------')
-# newPyramide.pyramideSearch(newPyramide.tree[4])
-# newPyramide.pyramideSearch(46064)
-# newPyramide.pyramideOutput()
-# print('-------')
-# newPyramide.pyramideRemove(newPyramide.tree[0])
-# newPyramide.pyramideRemove(46064)
-# newPyramide.pyramideOutput()
-# print('-------')
+    # Заготовка: для каждой итерации — свой массив и своя куча (создаются ДО таймера)
+    heaps = []
+    for _ in range(iterations):
+        arr = [random.randint(0, value_range) for _ in range(n)]
+        heap = Pyramide(arr, maximal=True)
+        heaps.append(heap)
 
-# ## min
-# newPyramide = Pyramide(arr, False)
-# newPyramide.pyramideOutput()
-# print('-------')
-# newPyramide.pyramideSearch(-newPyramide.tree[4])
-# newPyramide.pyramideSearch(46064)
-# newPyramide.pyramideOutput()
-# print('-------')
-# newPyramide.pyramideRemove(-newPyramide.tree[0])
-# newPyramide.pyramideRemove(46064)
-# newPyramide.pyramideOutput()
-# print('-------')
+    # Прогрев
+    _ = heaps[0].pyramideSort()
+    # Восстановим кучу после прогрева (чтобы состояние было как у остальных)
+    arr = [random.randint(0, value_range) for _ in range(n)]
+    heaps[0] = Pyramide(arr, maximal=True)
 
-# ## empty
-# newPyramide = Pyramide()
-# newPyramide.pyramideOutput()
-# newPyramide.pyramideRemove(46064)
-# newPyramide.pyramideSearch(46064)
-# print('-------')
+    # ЗАМЕР ТОЛЬКО pyramideSort()
+    start = time.perf_counter()
+    for heap in heaps:
+        _ = heap.pyramideSort()
+    end = time.perf_counter()
 
-# ## head only
-# newPyramide = Pyramide([5])
-# newPyramide.pyramideOutput()
-# newPyramide.pyramideSearch(46064)
-# print('-------')
+    total_time = end - start
+    avg_time = total_time / iterations
+
+    print(f"\n✅ Результаты:")
+    print(f"N = {n}, итераций = {iterations}")
+    print(f"Общее время: {total_time:.6f} сек")
+    print(f"Среднее время на pyramideSort(): {avg_time:.9f} сек")
 
 
-# ## sort
-# newPyramide = Pyramide(arr)
-# newPyramide.pyramideOutput()
-# sortedArr = newPyramide.pyramideSort()
-# print(sortedArr)
-
-# ## sort min
-# newPyramide = Pyramide(arr, False)
-# newPyramide.pyramideOutput()
-# sortedArr = newPyramide.pyramideSort()
-# print(sortedArr)
+if __name__ == "__main__":
+    benchmark_heap()
